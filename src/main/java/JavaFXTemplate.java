5b93f171ec2c24644b518946f894d845d4e6903a
juice176 <andreamunoz176@gmail.com>
2023-03-15 13:59:00 -0500

    update 1

--- src/main/java/JavaFXTemplate.java
+++ src/main/java/JavaFXTemplate.java
@@ -5,16 +5,34 @@ import javafx.animation.SequentialTransition;
 import javafx.application.Application;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.MenuItem;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 
 public class JavaFXTemplate extends Application {
-
+	Stage window;
+	Scene scene1,scene2,scene3,currentScene;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
@@ -24,36 +42,42 @@ public class JavaFXTemplate extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		 Rectangle rect = new Rectangle (100, 40, 100, 100);
-	     rect.setArcHeight(50);
-	     rect.setArcWidth(50);
-	     rect.setFill(Color.VIOLET);
+		primaryStage.setTitle("Welcome to Keno");
 
-	     RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
-	     rt.setByAngle(270);
-	     rt.setCycleCount(4);
-	     rt.setAutoReverse(true);
-	     SequentialTransition seqTransition = new SequentialTransition (
-	         new PauseTransition(Duration.millis(500)),
-	         rt
-	     );
-	     seqTransition.play();
-	     
-	     FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
-	     ft.setFromValue(1.0);
-	     ft.setToValue(0.3);
-	     ft.setCycleCount(4);
-	     ft.setAutoReverse(true);
+		window = primaryStage;
+		//scene1
+		VBox layout = new VBox(10);
+		scene1 = new Scene(layout,800,640);
 
-	     ft.play();
-	     BorderPane root = new BorderPane();
-	     root.setCenter(rect);
-	     
-	     Scene scene = new Scene(root, 700,700);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+		//Menu
+		Menu rulesMenu = new Menu("Rules");
+		Menu winOddsMenu = new Menu("Odds");
+		Menu exit = new Menu("Exit");
+
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(rulesMenu,winOddsMenu,exit);
+
+		HBox menu = new HBox(20);
+		menu.getChildren().add(menuBar);
+
+
+		Button button1 = new Button("Play");
+		button1.setOnAction(e -> window.setScene(scene2));
+
+		Label welcomeText = new Label("Welcome to Keno");
+		welcomeText.setAlignment(Pos.BASELINE_RIGHT);
+		welcomeText.setStyle("-fx-font: 40px \"Serif\";");
+
+		layout.setStyle("-fx-padding:10px;");
+		layout.getChildren().addAll(menu, welcomeText, button1);
+
+
+		window.setScene(scene1);
+		window.setTitle("KENO");
+		window.show();
+
+
+
 		
 				
 		
---
+++ src/main/java/Player.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+public class Player {
+    public static ArrayList<Integer> game;
+    public static int playBet;
+    public static int spot;
+    public static int totalWinnings;
+    //boolean gameRun = true;
+
+
+    // set player
+    Player(){
+        super();
+        this.game = new ArrayList<Integer>();
+        this.playBet = 0;
+        this.spot = 0;
+        this.totalWinnings = 0;
+    }
+    //setting the card by userinput
+    void storecardSelection(int value, boolean pressButton){
+        if(pressButton){
+            game.add(value);// store the numbers picked by the user
+        }
+        else{
+            game.remove(value); // remove the numbers not picked by the user
+        }
+    }
+    static boolean spotsSelect(String input){
+        int buttons = game.size();
+        if((input == "1" && buttons == 1) || (input == "4" && buttons == 4)||(input == "8" && buttons == 8)||(input == "10" && buttons == 10)){
+            return true;
+        }
+        return false;
+    }
+
+    static String getTotalWinning(){
+        return Integer.toString(totalWinnings);
+    }
+    static String getplayBet(){
+        return Integer.toString(playBet);
+    }
+    static void reset(){
+        game.clear();
+        playBet = 0;
+        spot = 0;
+        totalWinnings = 0;
+    }
+}
